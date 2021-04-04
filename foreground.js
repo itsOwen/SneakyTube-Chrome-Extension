d9d55ceb0996c4ce869805951f0cfb7f11f1cc64
itsOwen <owensingh72@gmail.com>
2021-04-04 16:41:27 +0530

    Fixed Multiple Reloading Imgs

--- foreground.js
+++ foreground.js
@@ -1,26 +1,58 @@
 // console.log('from foreground');
 {
-    let container = document.querySelector("#movie_player > div.html5-video-container")
+    let container = document.querySelector("#movie_player > div.html5-video-container ")
     let blackbg = document.querySelector("#movie_player > div.html5-video-container > video")
     let heading = document.querySelector("#container > h1");
     let orignalText = document.querySelector("#container > h1").innerText;
     let sideBar = document.querySelector("#items > ytd-item-section-renderer")
     const img = document.createElement("img")
-
     var randomNames = ["Chill Music video :)", "Why is Javascript and Python Important!", "Is Russia the most Powerful Country?"];
+    container.style.display = "flex"
+    container.style.flexDirection = "column"
+    container.style.justifyContent = "center"
+    container.style.alignItems = "center"
+    container.style.height = "100%"
+    img.style.display = "block"
+    img.setAttribute("class", "fakeImg")
+    var randomLink = "https://i.ibb.co/qWMmKHc/126679-28719-BC1-B2-A4-4632-AF68-F9-BC99667259-0-1.gif"
+    img.src = randomLink;
+
+    function removeAllImgs () {
+        container.childNodes.forEach(function(element) {
+            if(element.tagName === "IMG"){
+                element.remove();
+            }
+        })
+    }
+    
+    window.addEventListener("blur", function remove() {
+        removeAllImgs();
+        window.removeEventListener("blur", remove);
+    })
+    
+    window.addEventListener("focus", function add() {
+        img.src = randomLink;
+        container.prepend(img)
+        window.removeEventListener("focus", add);
+    })
+
 
     function videoBlack() {
-        var randomLink = ["http://scion-social.com/blog/wp-content/uploads/2020/11/gif-wallpaper-13.gif", "https://media.emailonacid.com/wp-content/uploads/2018/09/GifOutlook.gif"]
         const random = Math.floor(Math.random() * randomNames.length);
-        container.prepend(img)
+        
+        // container.prepend(img)
         var tgl = true;
 
         function toggle() {
-            const randomImage = Math.floor(Math.random() * randomLink.length);
             tgl ? blackbg.style.display = "none" : blackbg.style.display = "block";
             tgl ? heading.innerText = randomNames[random] : heading.innerText = orignalText;
             tgl ? sideBar.style.display = "none" : sideBar.style.display = "block";
-            img.src = randomLink[randomImage]
+            if(container.children[0].tagName === "VIDEO"){
+                container.prepend(img)
+            } else {
+                removeAllImgs();
+            }
+            
             tgl = !tgl;
         }
 
